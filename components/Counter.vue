4967441e45103c819655a7e201af90764858127c
Reza Masoumpour <reza.mas88@gmail.com>
2022-02-13 22:06:42 +0330

    Create Counter.vue

---
+++ components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">{{ min }} : {{ sec }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    days: Number,
+    hours: Number,
+    min: Number,
+    sec: Number,
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  font-size: 2.3rem;
+  letter-spacing: 3px;
+  color: #333;
+}
+</style>
